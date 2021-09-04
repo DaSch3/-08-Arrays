7052bd616d235d801886edc02204805334968b8f
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:26:30 +0200

    Projektdateien hinzufügen.

---
+++ Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1D-Arrays
+            int[] integerListe = new int[4];
+
+            integerListe[0] = 10;
+            integerListe[1] = 20;
+            integerListe[2] = 30;
+            integerListe[3] = 40;
+
+            Console.WriteLine(integerListe[1]);
+            Console.ReadKey();
+        }
+    }
+}
